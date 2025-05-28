e68e73d60dcc3fbc6e95d20b29a2f2211e262093
Agustina Gesualdo <agesualdo@itba.edu.ar>
2025-05-27 21:17:39 -0300

    Sexto commit

--- usuarioIngresado.js
+++ usuarioIngresado.js
@@ -5,7 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('saludo').innerHTML = `Hola ${usuario.nombre}!`;
         document.getElementById ('mail').innerHTML = `Mail: ${usuario.mail}`;
         document.getElementById('fecha').innerHTML = `Fecha de nacimiento: ${usuario.fecha}`;
-        document.getElementById ('contrasena').innerHTML = `Contraseña: ${usuario.contrasena}`;
+        let contrasenaReal = document.getElementById ('contrasena');
+        document.getElementById('botonMostrar').addEventListener('click', function (){
+            document.getElementById('contrasena').innerHTML = `Contraseña: ${usuario.contrasena}`;
+            botonMostrar.style.display = 'none';
+            botonOcultar.style.display = 'inline-block';
+        })
+        document.getElementById('botonOcultar').addEventListener('click', function (){
+            document.getElementById('contrasena').innerHTML = `Contraseña: ********`;
+            botonOcultar.style.display = 'none';
+            botonMostrar.style.display = 'inline-block';
+        })
     }
 });
 
